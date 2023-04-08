cec60b1e3f467a221545888c78d38c4afdd66dfd
thewecas <thewecas@gmail.com>
2023-04-09 00:04:35 +0530

    added cta on menu items

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-let count;
+let menu = document.getElementById("menu");
+let panel = document.getElementById("panel");
+let recipe = document.getElementById("recipe");
+let tag = document.getElementById("tag");
+let about = document.getElementById("about");
+let contact = document.getElementById("contact")
 
+let count;
 function initate() {
     count = 0;
+
 }
 
-let menu = document.getElementById("menu");
 function toggleMenu() {
     if (count == 0) {
         menu.classList.toggle("menu_open")
@@ -14,4 +20,68 @@ function toggleMenu() {
         menu.classList.toggle("menu_closed");
         menu.classList.toggle("menu_open")
     }
+}
+
+function toHome() {
+    toggleMenu();
+    showFlex(panel);
+    show(recipe);
+    hide(tag);
+    hide(about);
+    hide(contact);
+}
+function toRecipes() {
+    toggleMenu();
+    hide(panel);
+    show(recipe);
+    hide(tag);
+    hide(about);
+    hide(contact);
+}
+function toTags() {
+    toggleMenu();
+    hide(panel);
+    hide(recipe);
+    show(tag);
+    hide(about);
+    hide(contact);
+}
+function toAbout() {
+    toggleMenu();
+    hide(panel);
+    hide(recipe);
+    hide(tag);
+    hide(contact);
+    show(about);
+}
+function toContact() {
+    toggleMenu();
+    hide(panel);
+    hide(recipe);
+    hide(tag);
+    hide(about);
+    show(contact);
+}
+
+function toContactNoMenu() {
+    hide(panel);
+    hide(recipe);
+    hide(tag);
+    hide(about);
+    show(contact);
+}
+
+
+function hide(element) {
+    element.setAttribute("style", "display:none");
+
+}
+
+function show(element) {
+    element.setAttribute("style", "display:block");
+
+}
+function showFlex(element) {
+    element.setAttribute("style", "display:flex");
+
 }
